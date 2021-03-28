c9317465701540fa97c0bc777ec9d7d12a5e42c7
Bruno Fernandez <bruno.sistemas@outlook.com>
2021-03-28 12:30:37 -0300

    Added profile binding

--- app/src/main/java/br/com/hacksmiles/ui/home/HomeFragment.kt
+++ app/src/main/java/br/com/hacksmiles/ui/home/HomeFragment.kt
@@ -1,6 +1,26 @@
 package br.com.hacksmiles.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import br.com.hacksmiles.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater).apply {
+            setupView()
+        }
+        return binding.root
+    }
+
+    private fun FragmentHomeBinding.setupView() {
+    }
 }
--- app/src/main/java/br/com/hacksmiles/ui/profile/ProfileFragment.kt
+++ app/src/main/java/br/com/hacksmiles/ui/profile/ProfileFragment.kt
@@ -6,10 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.hacksmiles.R
+import br.com.hacksmiles.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    lateinit var binding: FragmentProfileBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater).apply {
+            setupView()
+        }
+        return binding.root
+    }
+
+    private fun FragmentProfileBinding.setupView() {
+
     }
 }
