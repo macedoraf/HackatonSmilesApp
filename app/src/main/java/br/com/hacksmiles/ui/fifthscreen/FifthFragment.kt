f3842896b20db965a9b7ad4bbb28d16cd8c8478d
Bruno Fernandez <bruno.sistemas@outlook.com>
2021-03-28 11:47:04 -0300

    Added some flows

--- app/src/main/java/br/com/hacksmiles/ui/fifthscreen/FifthFragment.kt
+++ app/src/main/java/br/com/hacksmiles/ui/fifthscreen/FifthFragment.kt
@@ -5,13 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import br.com.hacksmiles.R
+import br.com.hacksmiles.databinding.FragmentFifthBinding
+
 class FifthFragment : Fragment() {
 
+    lateinit var binding: FragmentFifthBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_fifth, container, false)
+    ): View {
+        binding = FragmentFifthBinding.inflate(inflater).apply {
+            setupView()
+        }
+        return binding.root
+    }
+
+    private fun FragmentFifthBinding.setupView() {
+        btNext.setOnClickListener {
+            findNavController().navigate(FifthFragmentDirections.actionFifthFragmentToSixthFragment())
+        }
     }
 }
