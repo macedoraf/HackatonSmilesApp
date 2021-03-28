8181632245dcc6a4197b392e9477cdbc40ef4a04
Bruno Fernandez <bruno.sistemas@outlook.com>
2021-03-28 00:30:46 -0300

    Created xml profile

--- app/src/main/java/br/com/hacksmiles/ui/profile/ProfileFragment.kt
+++ app/src/main/java/br/com/hacksmiles/ui/profile/ProfileFragment.kt
@@ -1,6 +1,15 @@
 package br.com.hacksmiles.ui.profile
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.hacksmiles.R
 
 class ProfileFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 }
