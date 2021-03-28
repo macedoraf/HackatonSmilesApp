62288fc56c6c9450ebafa6ff9cdf095a1504a624
Bruno Fernandez <bruno.sistemas@outlook.com>
2021-03-28 11:32:51 -0300

    Added some flows

--- app/src/main/java/br/com/hacksmiles/ui/fourthscreen/FourthFragment.kt
+++ app/src/main/java/br/com/hacksmiles/ui/fourthscreen/FourthFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import br.com.hacksmiles.R
+import androidx.navigation.fragment.findNavController
+import br.com.hacksmiles.databinding.FragmentFourthBinding
 
 class FourthFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var binding: FragmentFourthBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_fourth, container, false)
+    ): View {
+        binding = FragmentFourthBinding.inflate(inflater).apply {
+            setupView()
+        }
+        return binding.root
+    }
+
+    private fun FragmentFourthBinding.setupView() {
+        btnNext.setOnClickListener { findNavController().navigate(FourthFragmentDirections.actionFourthFragmentToFifthFragment()) }
     }
 }
